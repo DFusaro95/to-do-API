686ca8424291bdb66cadb7310c97d31f174cd96d
DiegoFusaro <diefusaro@gmail.com>
2022-11-18 00:14:18 -0300

    Actualizado app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //? Dependencies
 const express = require('express')
+const taskRouter = require('./tasks/tasks.router')
 
 //? Initial config
 const port = 9000
@@ -8,50 +9,13 @@ const app = express()
 //? Enable get JSON format
 app.use(express.json())
 
-
 app.get('/', (req, res) => {
   res.json({
     message: 'OK'
   })
 })
 
-//? /todo   /todo/:id
-//? Rutas de TO DO's
-app.get('/todo', (req, res) => {
-// Cuando me hagan una peticion a mi todoDB y sea exitosa, pasare en la respuesta un status 200 (OK)
-  res.status(200).json(todoDB)
-})
-
-app.post('/todo', (req, res) => {
-  const {title, description} = req.body
-
-  if(title && description){
-    const newToDo = {
-      id: id++,
-      title,
-      description,
-      is_completed: false
-    }
-  todoDB.push(newToDo)
-  res.status(200).json(newToDo)
-  } else {
-    res.status(400).json({
-      message: 'Invalid Data'
-    })
-  }
-})
-
-app.get('/todo/:id', (req, res) => {
-  const id = req.params.id
-
-  const todo = todoDB.find(item => item.id == id)
-
-  if (todo){
-    res.status(200).json(todo)
-  } else {
-    res.status(404).json({message: 'Invalid ID'})
-  }
-})
+app.use('/', taskRouter)
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`)
